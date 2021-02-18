39dd3eab75c79ed9a2a8fbc941c933ccd346ba85
Pipello <36567244+Pipello@users.noreply.github.com>
2021-02-18 20:05:35 +0100

    Update cover.jsx

---
+++ src/components/cover.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './cover.css';
+
+class Cover extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div className="min-vh-100" id="main_cover">
+            </div>
+        );
+    }
+}
+ 
+export default Cover;
