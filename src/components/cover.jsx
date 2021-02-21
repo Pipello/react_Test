63e33c090bf4bb0c26c3340df0a5ed2d39314cdc
Pipello <pipello@pips.home>
2021-02-21 14:44:21 +0100

    Pips Factory

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -5,7 +5,10 @@ class Cover extends Component {
     state = {  }
     render() { 
         return (  
-            <div className="min-vh-100" id="main_cover">
+            <div className="min-vh-100 min-vw-100" id="main_cover">
+                <div className="d-flex justify-content-center">
+                    <div className="ft-5" id="cover-title">Pip's Factory</div>
+                </div>
             </div>
         );
     }
