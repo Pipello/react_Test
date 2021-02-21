d6274e1d2855801c83e109bfa707779c9b2c9964
Pipello <pipello@pips.home>
2021-02-21 23:56:56 +0100

    Global style

--- src/components/cover.jsx
+++ src/components/cover.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './cover.css';
+import './global.css';
 
 class Cover extends Component {
-    state = {  }
+    state = { 
+    }
     render() { 
         return (  
             <div className="min-vh-100 min-vw-100" id="main_cover">
-                <div className="d-flex justify-content-center">
+                <div className="d-flex f-column justify-content-center top-1">
                     <div className="ft-5" id="cover-title">Pip's Factory</div>
+                    <div className="d-flex justify-content-center">
+                        <input class="form-control1 m-1 mw-50" id="search-bar" type="search" placeholder="Search..." aria-label="Search"></input>
+                    </div>
                 </div>
             </div>
         );
